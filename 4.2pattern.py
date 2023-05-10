500b45044b4b07f62d75d6e86c97697ed7145570
itsabhishekkr <132292924+itsabhishekkr@users.noreply.github.com>
2023-05-10 19:11:25 +0530

    python

---
+++ 4.2pattern.py
@@ -0,0 +1,55 @@
+# #*****
+# #****
+# #***
+# #**
+# #*
+# n=int(input())
+# i=1
+# m=n
+# while(i<=n):
+#     j=1
+#     while(j<=m):
+#         print("*",end="")
+#         j=j+1
+#     print()
+#     m=m-1
+#     i=i+1
+#*
+#**
+#***
+#****
+#*****
+#******
+m=int(input("inter number"))
+i=1
+while(i<=m):
+    spaces=1
+    while(spaces<=(m-1)):
+        print(" ",end="")
+        spaces=spaces+1
+    stars=1
+    while(stars<=i):
+        print("*",end="")
+        stars=stars+1
+    print()
+    i=i+1
+#
+                 
+
+
+
+
+
+# #   1
+# #  121
+# # 12321
+# #1234321
+# n=int(input())
+# i=1
+# while(i<=n):
+#     j=1
+#     while(j<=n-1):
+#         print("",end="")
+#         j=j+1
+
+        
